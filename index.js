1876542d7043cf147aadf0822a1137a5acfcb565
katherinemorgan-ut <katherinemorgan.ut@gmail.com>
2025-03-19 12:59:27 -0600

    adding an api for age of a certain name

--- index.js
+++ index.js
@@ -1,8 +1,50 @@
+require('dotenv').config();
+
+
+// Predict age from name
 (
     async () => {
-        const url = 
+        const url = 'https://api.agify.io';
+
+        //JSON response
+        const response = await fetch(url + "?" + new URLSearchParams({
+            name: 'katherine',
+        })
+        );
+
+        const data = await response.json();
+
+        console.log("'Katherine' is predicted to be " + data.age);
+
+    }
+) ();
 
 
-    } 
+// // Dad joke API 
+// (
+//     async () => {
+//         const url = 'https://icanhazdadjoke.com/';
 
-)();
+//         //JSON response
+//         const response = await fetch(url, {
+//             headers: {
+//                 'User-Agent':'My Library (https://github.com/katherinemorgan-ut/api-playground)',
+//                 'accept': 'application/json',
+//             },
+//         })
+
+//         const dadJoke = await response.json();
+//         console.log(dadJoke.joke);
+
+//         //Plain Text repsonse
+//         // const responsePlainText = await fetch(url, {
+//         //     headers: {
+//         //         'User-Agent':'My Library (https://github.com/katherinemorgan-ut/api-playground)',
+//         //         'accept': 'text/html',
+//         //     }
+//         // })
+
+//         // const secondDadJoke = response.body;
+//         // console.log(secondDadJoke);
+//     }
+// )();
